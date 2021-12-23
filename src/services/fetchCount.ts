3cc1a3714f679188d735031e9c7c199e2c543da7
yxj0312 <yanxiajun@hotmail.com>
2021-12-23 23:22:35 +0100

    add comment to fetchCount

--- src/services/fetchCount.ts
+++ src/services/fetchCount.ts
@@ -2,6 +2,7 @@ interface FetchCountCallback {
   (count: number): void
 }
 
+// it's accepting a callback function as a parameter, and call it here with the hard coded number
 export default function fetchCount(cb: FetchCountCallback) {
   cb(0)
 }
