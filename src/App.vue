f954870cfd4a5e87431423e525ff86862de96e29
yxj0312 <yanxiajun@hotmail.com>
2021-12-23 23:21:18 +0100

    add fetchCount

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-
-import { reactive, ref } from 'vue'
-
+import { ref, reactive, onMounted } from 'vue'
+import fetchCount from './services/fetchCount'
 interface AppInfo {
-  name: string
+  name: string,
   slogan: string
 }
-
-const count = ref(0)
-
+const count = ref<number | null>(null)
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
+})
+onMounted(() => {
+	fetchCount((initialCount) => {
+		count.value = initialCount
+	})
 })
 </script>
 
@@ -23,15 +22,5 @@ const appInfo: AppInfo = reactive({
     <h1>{{ appInfo.name }}</h1>
     <h2>{{ appInfo.slogan }}</h2>
   </div>
+	<p>{{ count }}</p>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
