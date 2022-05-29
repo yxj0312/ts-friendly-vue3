59e842aeb7a3798b8f7c84fbeee5fcce845f6e07
yxj0312 <yanxiajun@hotmail.com>
2022-05-29 22:59:25 +0200

    add method

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted, computed } from 'vue'
 import fetchCount from './services/fetchCount'
 interface AppInfo {
   name: string,
   slogan: string
 }
 const count = ref<number | null>(null)
+
+const nextCount = computed(()=>{
+  if (count.value !== null) {
+    return count.value + 1
+  }
+  return null
+})
+
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
