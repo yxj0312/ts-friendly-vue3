2c6ae6d4514d9f1877de4639840ffb9590072b44
yxj0312 <yanxiajun@hotmail.com>
2022-06-01 22:34:30 +0200

    extract counter.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted, computed } from 'vue'
-import fetchCount from './services/fetchCount'
+import { reactive  } from 'vue'
+import Counter from './components/Counter.vue';
+
 interface AppInfo {
   name: string,
   slogan: string
 }
-const count = ref<number | null>(null)
-
-const nextCount = computed(()=>{
-  if (count.value !== null) {
-    return count.value + 1
-  }
-  return null
-})
 
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on'
 })
-onMounted(() => {
-	fetchCount((initialCount) => {
-		count.value = initialCount
-	})
-})
 
-function addCount(num: number) {
-  if (count.value !== null) {
-    count.value += num
-  }
-}
 </script>
 
 <template>
@@ -36,6 +19,5 @@ function addCount(num: number) {
     <h1>{{ appInfo.name }}</h1>
     <h2>{{ appInfo.slogan }}</h2>
   </div>
-	<p>{{ count }}</p>
-  <p><button @click="addCount(1)">Add</button></p>
+  <Counter></Counter>
 </template>
